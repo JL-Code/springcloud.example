52b3876fdba7a964144162742e0879679b3ef107
蒋勇 <jiangy@highzap.com>
2020-03-12 16:09:04 +0800

    init project

---
+++ springcloud-payment/src/main/java/com/codeme/springcloud/payment/PaymentMain.java
@@ -0,0 +1,19 @@
+package com.codeme.springcloud.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>描述: 启动类 </p>
+ * <p>创建时间: 2020/3/12 </p>
+ *
+ * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
+ * @version v1.0
+ */
+@SpringBootApplication
+public class PaymentMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}

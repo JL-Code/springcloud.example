a3bb7321cff6d174769266f6a4185637dbec5e03
蒋勇 <jiangy@highzap.com>
2020-03-29 16:30:11 +0800

    添加枚举测试

---
+++ springcloud-commons/src/main/java/com/codeme/springcloud/commons/enums/TestType.java
@@ -0,0 +1,9 @@
+package com.codeme.springcloud.commons.enums;
+
+public enum TestType {
+    GZ,ZG;
+
+    TestType() {
+        System.out.println("丑陋的公知");
+    }
+}
---
+++ springcloud-commons/src/test/java/TestEnum.java
@@ -0,0 +1,11 @@
+import com.codeme.springcloud.commons.enums.TestType;
+import org.junit.jupiter.api.Test;
+
+public class TestEnum {
+    @Test
+    void testEnumConstructorCall() {
+        int a= 4;
+        Integer b =66;
+        System.out.println(TestType.GZ);
+    }
+}

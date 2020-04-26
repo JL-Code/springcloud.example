474a16fd57aa2b3e66b6dd9665cee8a5ba3fe05e
蒋勇 <jiangy@highzap.com>
2020-04-26 20:44:54 +0800

    init springcloud-nacos

---
+++ springcloud-nacos/src/test/java/com/codeme/springcloud/nacos/SpringcloudNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeme.springcloud.nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudNacosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

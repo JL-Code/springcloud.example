a2d7b0367126553ffd10d8b7d284331df54ec75f
蒋勇 <jiangy@highzap.com>
2020-05-08 09:39:47 +0800

    init seata project

---
+++ springcloud-seata/src/test/java/com/codeme/springcloud/seata/SpringcloudSeataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeme.springcloud.seata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudSeataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

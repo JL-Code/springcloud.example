a8964c32bb69ca56b9e0936f78514e40a3f1ae3b
蒋勇 <jiangy@highzap.com>
2020-10-31 21:02:25 +0800

    inetutils ignored-interfaces 相关属性放置到 bootstrap.yml 初始（程序已启动就读取）

--- springcloud-payment/src/main/java/com/codeme/springcloud/payment/PaymentMain.java
+++ springcloud-payment/src/main/java/com/codeme/springcloud/payment/PaymentMain.java
@@ -2,7 +2,7 @@ package com.codeme.springcloud.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * <p>描述: 启动类 </p>
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @version v1.0
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain {
 
     public static void main(String[] args) {
--- springcloud-ribbon-order/src/main/java/com/codeme/springcloud/ribbonorder/RibbonOrderMain.java
+++ springcloud-ribbon-order/src/main/java/com/codeme/springcloud/ribbonorder/RibbonOrderMain.java
@@ -2,12 +2,12 @@ package com.codeme.springcloud.ribbonorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
-// 启用 Feign 注解
+@EnableDiscoveryClient
 @EnableFeignClients
 public class RibbonOrderMain {
     public static void main(String[] args) {

22ed058efec06a47f007c6c9301d7213f14e071d
蒋勇 <jiangy@highzap.com>
2021-01-26 01:47:19 +0800

    修改 logback.xml  logstash.conf，完成动态创建 es index

--- springcloud-elk-poc/src/main/java/com/codeme/springcloud/order/OrderMain.java
+++ springcloud-elk-poc/src/main/java/com/codeme/springcloud/order/OrderMain.java
@@ -2,8 +2,8 @@ package com.codeme.springcloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * <p>描述: [类型描述] </p>
@@ -12,9 +12,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
  * @version v1.0
  */
-@EnableEurekaClient
+//@EnableEurekaClient
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class, args);
--- springcloud-elk-poc/src/main/java/com/codeme/springcloud/order/controller/LogstashController.java
+++ springcloud-elk-poc/src/main/java/com/codeme/springcloud/order/controller/LogstashController.java
@@ -1,8 +1,29 @@
-package com.codeme.springcloud.order.controller;/**
+package com.codeme.springcloud.order.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * <p>创建时间: 2021/1/25 </p>
  *
  * @author <a href="mailto:jiangy@highzap.com" rel="nofollow">蒋勇</a>
  * @version v1.0
  */
+@Slf4j
+@RestController
 public class LogstashController {
+
+    @GetMapping("/log")
+    public String getLog(String message) {
+        log.info(message);
+        return "ok";
+    }
+
+    @PostMapping("/log")
+    public String postLog(String message) {
+        log.error(message);
+        return "ok";
+    }
 }
